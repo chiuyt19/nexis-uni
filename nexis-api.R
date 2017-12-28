5fb6f5ee3c2eca4d7d02d19be75e5d93023ea0a4
Cindy <chiuyt19@uw.edu>
2017-12-28 12:04:31 -0800

    add firm

--- nexis-api.R
+++ nexis-api.R
@@ -2,3 +2,8 @@ library(dplyr)
 library(httr)
 library(jsonlite)
 
+basic.url <-"http://advance.lexis.com/api/"
+
+#Chinese unicorn firm with 255 news article in the GUI search
+search.term <- "UrWork"
+
